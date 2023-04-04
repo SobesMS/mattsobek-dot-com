cf950aa501209241ecd4e800f6916fec1a7e978f
Matt Sobek <sobesms@gmail.com>
2023-04-04 16:58:46 -0400

    first version email form

--- scripts/send-email.php
+++ scripts/send-email.php
@@ -1,13 +1,45 @@
 <?php
-if($_POST['submit']) {
-    $email_to = "sobesms@gmail.com";
-    $email_subject = "New Email From mattsobek.com";
+    function IsInjected($str) {
+        $injections = array(
+            '(\n+)',
+            '(\r+)',
+            '(\t+)',
+            '(%0A+)',
+            '(%0D+)',
+            '(%08+)',
+            '(%09+)'
+        );
 
-    $fname = $_POST['fname2'];
-    $lname = $_POST['lname2'];
-    $email = $_POST['email2'];
-    $message = $_POST['message2'];
+        $inject = join('|', $injections);
+        $inject = "/$inject/i";
 
-    $email_content = "From: ".$fname." ".$lname."\nEmail: ".$email."\n\n".$message;
-    mail($email_to, $email_subject, $email_content);
-}
+        if(preg_match($inject, $str)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    if(!empty($_POST['submit'])) {
+        $email_to = "sobesms+msdc@gmail.com";
+        $email_subject = "Form Submission From mattsobek.com";
+
+        $fname = $_POST['fname2'];
+        $lname = $_POST['lname2'];
+        $email_from = $_POST['email2'];
+        $message = $_POST['message2'];
+
+
+        $email_content = "From: ".$fname." ".$lname."\nEmail: ".$email_from."\n\n".$message;
+        $email_headers = "From: ".$email_from."\r\n";
+
+        if(IsInjected($fname) | IsInjected($lname) | IsInjected($email_from) | IsInjected($message)) {
+            echo "Invalid submission data. Please try again.";
+            exit;
+        }
+        
+        mail($email_to, $email_subject, $email_content, $email_headers);
+    } else {
+        echo "An error occurred.";
+    }
+?>
