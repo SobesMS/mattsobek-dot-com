f1aa2a72ecc65ceddf89dd7aa58423c8243566c2
Matt Sobek <sobesms@gmail.com>
2023-04-07 23:42:52 -0400

    more thank you tweaks

--- php/send-email.php
+++ php/send-email.php
@@ -12,11 +12,11 @@
             "message" => $_POST['message']
         ];
                 
-        $in_content = "From: ".$user["fname"]." ".$user["lname"]."\nEmail: ".$user["email"]."\nMessage:\n".$user["message"];
+        $in_content = "From: ".$user["fname"]." ".$user["lname"]."\nEmail: ".$user["email"]."\n\nMessage:\n".$user["message"];
         $in_headers = "From: ".$user["email"]."\r\n";
 
         $out_subject = "Thank you from MattSobek.com";
-        $out_content = "Your message has been received and is greatly appreciated. I will respond to inquiries as soon as possible.\n\nRegards,\nMatt Sobek";
+        $out_content = "Your message has been received and is greatly appreciated. The contents of your email are as follows:\n\n \"".$user["message"]."\"\n\nI will respond to inquiries as soon as possible. Have a great day!\n\nRegards,\nMatt Sobek";
 
         mail($site["email"], $site["subject"], $in_content, $in_headers);
         mail($user["email"], $out_subject, $out_content, "From: noreply@mattsobek.com\r\n");
